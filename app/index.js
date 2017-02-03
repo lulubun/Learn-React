8f13ef0d314f7deeeee2651335a1ebb753b70452
lulubun <laurapetitt@gmail.com>
2017-02-03 00:25:56 -0500

    issues loading page

--- app/index.js
+++ app/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-import AppLayout from './layouts/AppLayout'
-import Users from './components/Users'
-import UserProfile from './components/UserProfile'
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={AppLayout}>
-      <IndexRoute component={Users} />
-      <Route path="/profile/:id" component={UserProfile} />
-    </Route>
-  </Router>
-), document.getElementById('root'))
+let User = React.createClass({
+	render: function() {
+		return <div>UserL</div>
+	}
+})
+
+let App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<h1>User List:</h1>
+				<User />
+			</div>
+		)	
+	}
+})
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
